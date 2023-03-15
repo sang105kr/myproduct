730c81d9f43fdc69e8c2c0d01cc92abe1d1f85f0
sanghakyi <sang105kr@gmail.com>
2023-03-15 17:05:08 +0900

    'springboot_base_project'

---
+++ src/main/java/com/kh/myproduct/myproduct/MyproductApplication.java
@@ -0,0 +1,13 @@
+package com.kh.myproduct.myproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyproductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyproductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/myproduct/myproduct/MyproductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.myproduct.myproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyproductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
