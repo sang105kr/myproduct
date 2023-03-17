5879bc403d4ec07efc167b8d76d0308f9d5e7f75
sanghakyi <sang105kr@gmail.com>
2023-03-17 12:49:49 +0900

    '20230317-04'

--- src/main/java/com/kh/myproduct/dao/Product.java
+++ src/main/java/com/kh/myproduct/dao/Product.java
@@ -1,8 +1,12 @@
 package com.kh.myproduct.dao;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
+@Data //@ToString, @EqualsAndHashCode, @Getter,  @Setter, @RequiredArgsConstructor
+@AllArgsConstructor //모든멤버필드를 매개변수로하는 생성자 생성
+@NoArgsConstructor  //디폴트생성자
 public class Product {
   private Long productId; // PRODUCT_ID	NUMBER(10,0)
   private String pname;   //PNAME	VARCHAR2(30 BYTE)
